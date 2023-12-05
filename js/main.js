1acfa3a26c4de7b7d29882ed47c4f973f5545823
SquallStrifeX <squallstrifex@gmail.com>
2023-12-05 17:43:32 +0100

    end milestone 2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+let next = document.getElementById('nextbutton');
+let prev = document.getElementById('prevbutton');
+let items = document.querySelector('.items');
+let imagex = document.getElementById('image');
+let titlex = document.getElementById('title');
+let textx = document.getElementById('text')
+let corrente = 0;
+
+function showimage(index) {
+    const {image, title, text} = images[index];
+    imagex.src=image;
+    titlex.textContent=title;
+    textx.textContent=text;
+
+}
+
+//     let giocoCorrente = images[corrente];
+//     items.innerHTML = `<div class="item active">
+//         <img src="${giocoCorrente.image}" alt="">
+//         <div class="position-absolute bottom-50 text-right color-white padding-text">
+//             <h2>${giocoCorrente.title}</h2>
+//             <h5>${giocoCorrente.text}</h5>
+//         </div>
+//     </div> 
+
+//     <div class="prev" id="prev"></div>
+//     <div class="next" id="next"></div>`;
+// }
+
+function nextGame() {
+    corrente = (corrente + 1) % images.length;
+    showimage(corrente);
+}
+
+function prevGame() {
+    corrente = (corrente - 1 + images.length) % images.length;
+    showimage(corrente);
+}
+
+next.addEventListener('click', nextGame);
+prev.addEventListener('click', prevGame);
+
+showimage(corrente);
